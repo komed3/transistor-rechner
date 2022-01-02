7ee80d365e306bd2354dfcc64794354355c7da8e
Paul Köhler <pk97@outlook.de>
2022-01-02 09:55:39 +0100

    add first content

---
+++ wp-content/plugins/transistor-rechner/transistor-rechner.php
@@ -0,0 +1,16 @@
+<?php
+
+    /*
+     * Plugin Name: Transistor-Rechner.de
+     * Plugin URI: https://github.com/komed3/transistor-rechner
+     * Description: Transistor-Rechner.de plugin and theme bundle
+     * Author: Paul Köhler (komed3)
+     * Author URI: https://labs.komed3.de
+     * Version: 0.01 [BETA] @ 2022-01-02
+     * License: MIT
+     * Text Domain: transistor
+     */
+
+    require_once __DIR__ . '/src/functions.php';
+
+?>
