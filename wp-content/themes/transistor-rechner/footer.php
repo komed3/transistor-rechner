20982a9d75ab8d2c8047bbf199a2a64d16051545
komed3 <pk97@outlook.de>
2022-01-02 16:45:25 +0100

    footer

--- wp-content/themes/transistor-rechner/footer.php
+++ wp-content/themes/transistor-rechner/footer.php
@@ -1,6 +1,23 @@
 </main>
                 <footer id="footer">
                     <div class="footer-inner">
+                        <nav>
+                            <?php wp_nav_menu( [
+                                'theme_location' => 'footer',
+                                'container' => '',
+                                'fallback_cb' => false
+                            ] ); ?>
+                        </nav>
+                        <div class="credits">
+                            <?php printf(
+                                __( '<a href="%s">%s</a> &copy; %s by <a href="%s">%s</a>. All rights reserved', 'transistor' ),
+                                home_url( '/' ),
+                                get_bloginfo( 'title' ),
+                                wp_date( 'Y' ),
+                                'https://github.com/komed3',
+                                'komed3'
+                            ); ?>
+                        </div>
                     </div>
                 </footer>
             </div>
