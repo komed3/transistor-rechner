a45badaa6de5cbd7119234176bd91f5d3e7d7560
Paul Köhler <pk97@outlook.de>
2022-01-02 23:49:45 +0100

    add attachment image

--- wp-content/themes/transistor-rechner/functions.php
+++ wp-content/themes/transistor-rechner/functions.php
@@ -18,6 +18,8 @@
 
         add_theme_support( 'title-tag' );
 
+        add_theme_support( 'post-thumbnails' );
+
     }
 
     add_action( 'after_setup_theme', '__theme_setup' );
--- wp-content/themes/transistor-rechner/page.php
+++ wp-content/themes/transistor-rechner/page.php
@@ -1,5 +1,8 @@
 <?php get_header(); ?>
 <article class="<?php echo implode( ' ', get_post_class() ); ?>">
+    <?php if( has_post_thumbnail() ) { ?>
+        <div class="attachment" style="background-image: url( <?php the_post_thumbnail_url( 'large' ); ?> );"></div>
+    <?php } ?>
     <h1><?php the_title(); ?></h1>
     <div class="body">
         <?php the_content(); ?>
