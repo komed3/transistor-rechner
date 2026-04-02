648f7d2e848a6ffe022680e1f4a1a35985003b33
komed3 <pk97@outlook.de>
2026-04-02 02:18:08 +0200

    apply custor cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
+import { CustomCursor } from './components/CustomCursor';
+
+
 export default function App () {
-    return ( <></> );
+    return ( <div className="relative min-h-screen flex flex-col p-4 md:p-12 bg-alu-bg text-alu-white overflow-x-hidden">
+        <CustomCursor />
+    </div> );
 }
