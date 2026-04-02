a5768c9e0cd47bfb88bcb4ab931a1cf732efe38d
komed3 <pk97@outlook.de>
2026-04-02 02:20:21 +0200

    add circuit background pattern

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { CircuitBackground } from './components/CircuitBackground';
 import { CustomCursor } from './components/CustomCursor';
 
 
 export default function App () {
     return ( <div className="relative min-h-screen flex flex-col p-4 md:p-12 bg-alu-bg text-alu-white overflow-x-hidden">
+        <CircuitBackground />
         <CustomCursor />
     </div> );
 }
